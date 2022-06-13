1994c1d01aaf75034b20b6ad13b71a1ce67cb562
Edmund <pieniu1511@gmail.com>
2022-06-13 20:28:00 +0200

    nav improved

--- js/main.js
+++ js/main.js
@@ -1,18 +1,31 @@
 const navMobile = document.querySelector('.nav-mobile');
 const navBtn = document.querySelector('.hamburger');
 const footerYear = document.querySelector('.footer__year');
+const navLinks = document.querySelectorAll('.nav__link')
 
 const handleNav = () => {
-    navBtn.classList.toggle('is-active');
-    navMobile.classList.toggle('nav-mobile--active');
-}
+	navBtn.classList.toggle('is-active');
+	navMobile.classList.toggle('nav-mobile--active');
+
+    navLinks.forEach(item => {
+		item.addEventListener('click', () => {
+            navBtn.classList.remove('is-active');
+			navMobile.classList.remove('nav-mobile--active')
+		})
+	})
+};
+
+// const closeNav = () => {
+// 	navBtn.classList.remove('is-active');
+// 	navMobile.classList.remove('nav-mobile--active');
+// };
 
 navBtn.addEventListener('click', handleNav);
+// navLink.addEventListener('click', closeNav);
 
 const handleCurrentYear = () => {
-	const year = new Date().getFullYear()
-	footerYear.innerText = year
-}
+	const year = new Date().getFullYear();
+	footerYear.innerText = year;
+};
 
 handleCurrentYear();
-
